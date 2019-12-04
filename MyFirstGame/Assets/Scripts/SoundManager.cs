611ac496a7442066ec4bb7304264bd1810f2bc2b
JuanjoMedina <juanjomedinamusellas@gmail.com>
2019-12-04 01:01:40 +0100

    First Commit

---
+++ MyFirstGame/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+    public static SoundManager instance = null;
+    public float lowPichRange = .95f;
+    public float highPichRange = 1.05f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+    public void PlaySingle (AudioClip clip)
+    {
+        efxSource.clip = clip;
+        efxSource.Play();
+    }
+    public void RandomizeSfx(params AudioClip [] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPich = Random.Range(lowPichRange, highPichRange);
+
+        efxSource.pitch = randomPich;
+        efxSource.clip = clips[randomIndex];
+        efxSource.Play();
+
+    }
+
+}
